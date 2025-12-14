014985da0429591b54fb66a37c5e86a2a1f935a0
Nikita Paniukhin <git@npanuhin.me>
2025-12-15 00:10:04 +0100

    Solution Day 1

--- src/day1/FineGrainedBank.kt
+++ src/day1/FineGrainedBank.kt
@@ -4,44 +4,57 @@ package day1
 
 import day1.Bank.Companion.MAX_AMOUNT
 import java.util.concurrent.locks.*
+import kotlin.concurrent.withLock
 
 class FineGrainedBank(accountsNumber: Int) : Bank {
     private val accounts: Array<Account> = Array(accountsNumber) { Account() }
 
     override fun getAmount(id: Int): Long {
-        // TODO: Make this operation thread-safe via fine-grained locking.
         val account = accounts[id]
-        return account.amount
+        return account.lock.withLock {
+            account.amount
+        }
     }
 
     override fun deposit(id: Int, amount: Long): Long {
-        // TODO: Make this operation thread-safe via fine-grained locking.
         require(amount > 0) { "Invalid amount: $amount" }
         val account = accounts[id]
-        check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
-        account.amount += amount
-        return account.amount
+        return account.lock.withLock {
+            check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
+            account.amount += amount
+            account.amount
+        }
     }
 
     override fun withdraw(id: Int, amount: Long): Long {
-        // TODO: Make this operation thread-safe via fine-grained locking.
         require(amount > 0) { "Invalid amount: $amount" }
         val account = accounts[id]
-        check(account.amount - amount >= 0) { "Underflow" }
-        account.amount -= amount
-        return account.amount
+        return account.lock.withLock {
+            check(account.amount - amount >= 0) { "Underflow" }
+            account.amount -= amount
+            account.amount
+        }
     }
 
     override fun transfer(fromId: Int, toId: Int, amount: Long) {
-        // TODO: Make this operation thread-safe via fine-grained locking.
         require(amount > 0) { "Invalid amount: $amount" }
         require(fromId != toId) { "fromId == toId" }
-        val from = accounts[fromId]
-        val to = accounts[toId]
-        check(amount <= from.amount) { "Underflow" }
-        check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
-        from.amount -= amount
-        to.amount += amount
+
+        val firstId = minOf(fromId, toId)
+        val secondId = maxOf(fromId, toId)
+        val first = accounts[firstId]
+        val second = accounts[secondId]
+
+        first.lock.withLock {
+            second.lock.withLock {
+                val from = accounts[fromId]
+                val to = accounts[toId]
+                check(amount <= from.amount) { "Underflow" }
+                check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
+                from.amount -= amount
+                to.amount += amount
+            }
+        }
     }
 
     /**
@@ -53,9 +66,6 @@ class FineGrainedBank(accountsNumber: Int) : Bank {
          */
         var amount: Long = 0
 
-        /**
-         * TODO: use this mutex to protect the account data.
-         */
         val lock = ReentrantLock()
     }
 }
--- src/day1/TreiberStackWithElimination.kt
+++ src/day1/TreiberStackWithElimination.kt
@@ -6,8 +6,6 @@ import java.util.concurrent.atomic.*
 open class TreiberStackWithElimination<E> : Stack<E> {
     private val stack = TreiberStack<E>()
 
-    // TODO: Try to optimize concurrent push and pop operations,
-    // TODO: synchronizing them in an `eliminationArray` cell.
     private val eliminationArray = AtomicReferenceArray<Any?>(ELIMINATION_ARRAY_SIZE)
 
     override fun push(element: E) {
@@ -16,24 +14,44 @@ open class TreiberStackWithElimination<E> : Stack<E> {
     }
 
     protected open fun tryPushElimination(element: E): Boolean {
-        TODO("Implement me!")
-        // TODO: Choose a random cell in `eliminationArray`
-        // TODO: and try to install the element there.
-        // TODO: Wait `ELIMINATION_WAIT_CYCLES` loop cycles
-        // TODO: in hope that a concurrent `pop()` grabs the
-        // TODO: element. If so, clean the cell and finish,
-        // TODO: returning `true`. Otherwise, move the cell
-        // TODO: to the empty state and return `false`.
+        val index = randomCellIndex()
+
+        if (!eliminationArray.compareAndSet(index, CELL_STATE_EMPTY, element)) return false
+
+        repeat(ELIMINATION_WAIT_CYCLES) {
+            if (eliminationArray.get(index) === CELL_STATE_RETRIEVED) {
+                eliminationArray.set(index, CELL_STATE_EMPTY)
+                return true
+            }
+        }
+
+        if (eliminationArray.compareAndSet(index, element, CELL_STATE_EMPTY)) {
+            return false
+        }
+
+        eliminationArray.compareAndSet(index, CELL_STATE_RETRIEVED, CELL_STATE_EMPTY)
+        return true
     }
 
     override fun pop(): E? = tryPopElimination() ?: stack.pop()
 
     private fun tryPopElimination(): E? {
-        TODO("Implement me!")
-        // TODO: Choose a random cell in `eliminationArray`
-        // TODO: and try to retrieve an element from there.
-        // TODO: On success, return the element.
-        // TODO: Otherwise, if the cell is empty, return `null`.
+        val index = randomCellIndex()
+
+        return when (val cur = eliminationArray.get(index)) {
+            CELL_STATE_EMPTY -> null
+            CELL_STATE_RETRIEVED -> {
+                eliminationArray.compareAndSet(index, CELL_STATE_RETRIEVED, CELL_STATE_EMPTY)
+                null
+            }
+
+            else -> if (eliminationArray.compareAndSet(index, cur, CELL_STATE_RETRIEVED)) {
+                @Suppress("UNCHECKED_CAST")
+                return cur as E
+            } else {
+                null
+            }
+        }
     }
 
     private fun randomCellIndex(): Int =
